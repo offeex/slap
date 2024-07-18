b2daa234aa62adf8ca3b1646d592c5c55b775cd2
offeex <rockezprod@gmail.com>
2024-07-18 16:49:07 +0300

    added fish completions, refactored the code

--- main.go
+++ main.go
@@ -19,14 +19,23 @@ Examples:
   slap /path/to/directory/   # Creates a directory (ends with /) `
 
 	app := &cli.App{
-		Name:            "slap",
-		Usage:           "Modern replacement for touch and mkdir commands",
-		HideHelpCommand: true,
-		UsageText:       usageText,
-		Description:     description,
+		Name:                 "slap",
+		Usage:                "Modern replacement for touch and mkdir commands",
+		HideHelpCommand:      true,
+		UsageText:            usageText,
+		Description:          description,
+		Version:              "0.1",
+		EnableBashCompletion: true,
+
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  "cd",
+				Usage: "cd into the created directory",
+			},
+		},
 
 		Action: func(c *cli.Context) error {
-			if c.NArg() != 1 {
+			if c.NArg() < 1 {
 				return fmt.Errorf("expected 1 path argument, but got %d", c.NArg())
 			}
 
@@ -35,18 +44,29 @@ Examples:
 				return fmt.Errorf("path argument is empty")
 			}
 
-			return create(pathArg)
+			err := create(pathArg)
+			if err != nil {
+				return err
+			}
+
+			return nil
 		},
 	}
 
-	err := app.Run(os.Args)
-	if err != nil {
+	genFishCompletions(app.ToFishCompletion())
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatalln(err)
 	}
 }
 
 func create(fullPath string) error {
 	dirPath := filepath.Dir(fullPath)
+
+	if dirPath == "" {
+		dirPath = "./"
+	}
+
 	if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
 		return err
 	}
@@ -68,4 +88,20 @@ func create(fullPath string) error {
 
 	fmt.Printf("%s file: %s\n", actionText, fullPath)
 	return nil
+}
+
+func genFishCompletions(completion string, err error) {
+	if err != nil {
+		fmt.Printf("Failed to generate fish completion script: %v\n", err)
+		return
+	}
+
+	completionDir := filepath.Join(os.Getenv("HOME"), ".config", "fish", "completions")
+	completionFile := filepath.Join(completionDir, "slap.fish")
+
+	err = os.WriteFile(completionFile, []byte(completion), 0644)
+	if err != nil {
+		fmt.Printf("Failed to write completion file: %v\n", err)
+		return
+	}
 }
